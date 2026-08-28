58db2fe9fb7c53aa03f4019ea7f86adf068d68ee
shoaibrrian <shoaibrian@gmail.com>
2026-08-29 01:24:57 +0600

    feat(database): implement MongoDB connection setup in db.js and integrate with server

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,16 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+await connectDB();
 
 // Middleware
 app.use(
